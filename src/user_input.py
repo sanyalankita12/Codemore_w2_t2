468c8e90e1b9277c36493f4202eaf6e6b1323c95
sanyalankita12 <sanyal.ankita12@gmail.com>
2026-06-25 19:17:59 +0530

    Updating the user input file and screenshots

--- src/user_input.py
+++ src/user_input.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+
 def greeting():
     name = input("What is your name? ")
     age = int(input("How old are you? "))
-    fav_language = input("What is your favourite language? ")
+    fav_language = input("What is your favourite programming language? ")
     difference=100-age
-    year=difference+2026
+    current_year = datetime.now().year
+    year = current_year + (100 - age)
     print(f"Hello,{name}! You're {age} years old. And your favourite programming language is {fav_language}.")
     print(f"You will turn 100 in year {year}")
 
